be574dbc708c91e6fdd408d4b9b03cdef4262f7d
eesooss <eesooss@icloud.com>
2023-02-07 20:33:36 +0200

    String builder is added

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -1,16 +1,30 @@
 import java.util.Random;
 
 public class RandomWord {
-    private String[] words = {"father", "mother", "brother", "family", "sister", "friend", "likeness", "illness"};
-
-    private Random random = new Random();
+    private String[] words = {"father", "mother", "brother", "eye", "family", "sister", "friend", "likeness", "illness"};
     private String chosenWord;
+    private Random random = new Random();
+    private char[] characters;
 
     public RandomWord() {
         chosenWord = words[random.nextInt((words.length))];
+        characters = new char[chosenWord.length()];
     }
 
     public String toString() {
-        return chosenWord;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : characters) {
+            if (c == '\u0000') {
+                sb.append('_');
+            } else {
+                sb.append(c);
+            }
+
+            sb.append(' ');
+        }
+
+        return sb.toString();
     }
 }
