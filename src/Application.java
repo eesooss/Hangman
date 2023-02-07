5224914d7a445424efb809721097a6ad13bfd72a
eesooss <eesooss@icloud.com>
2023-02-07 18:08:14 +0200

    Project is created

---
+++ src/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+        var game = new Hangman();
+
+        game.run();
+    }
+}
---
+++ src/Hangman.java
@@ -0,0 +1,5 @@
+public class Hangman {
+    void run() {
+
+    }
+}
